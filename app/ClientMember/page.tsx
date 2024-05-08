b7195f5327cd1bf2d40e1a9a44219bd61d32c19e
Le Duc Long <lelongrvp7@gmail.com>
2024-05-08 17:45:16 +0700

    feat: setup middleware for role-based authorization

--- app/ClientMember/page.tsx
+++ app/ClientMember/page.tsx
@@ -1,9 +1,23 @@
-"use client"
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 const ClientMember = () => {
-  return (
-    <div>Client Member Session</div>
-  )
-}
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin/callbackUrl=/ClientMember");
+    },
+  });
 
-export default ClientMember
+  return (
+    <div>
+      <h1>Client Member Session</h1>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+    </div>
+  );
+};
+
+export default ClientMember;
